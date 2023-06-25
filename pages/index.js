54cbfd5028792ec11df8a5c9d0359d11a39364ec
Giovanni Sena <giovanni.sena@outlook.com>
2023-06-25 14:34:35 -0300

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return <h1>Jeizi, eu te amo 💓!<br /><br /> 
+    
+    Não desista dos seus objetivos por achar ser difícil, pois não é!<br /><br /> 
+
+    Você me mostrou que o difícil se torna fácil, pois você me ensinou que tudo é possível de se realizar!!!</h1>
+}
+
+export default Home;
