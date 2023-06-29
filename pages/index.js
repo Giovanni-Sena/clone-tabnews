dc817f98eb46b87ec116640d10d26d0c775483c6
Giovanni Sena <giovanni.sena@outlook.com>
2023-06-29 07:49:23 -0300

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home(){
     
     Não desista dos seus objetivos por achar ser difícil, pois não é!<br /><br /> 
 
-    Você me mostrou que o difícil se torna fácil, pois você me ensinou que tudo é possível de se realizar!!!</h1>
+    Você me mostrou que o difícil se torna fácil, pois você me ensinou que tudo é possível de se realizar!!</h1>
 }
 
 export default Home;
